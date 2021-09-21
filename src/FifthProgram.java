42e3d86f49b0bf0c0ba74cbdd1f48b5954b633ef
Shivang Sudhakar <shivangsudhakar999@gmail.com>
2021-09-21 08:07:08 +0530

    Product of 2 numbers

---
+++ src/FifthProgram.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class FifthProgram {
+    public static void main(String[] args) {
+        float a,b;
+        Scanner s=new Scanner(System.in);
+        a=s.nextFloat();
+        b=s.nextFloat();
+
+        Float c= a*b;
+        System.out.println(c);
+
+    }
+}
