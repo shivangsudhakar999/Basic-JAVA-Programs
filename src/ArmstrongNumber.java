cb29006d0879f47c5b56c15328dc57848914d6c5
Shivang Sudhakar <shivangsudhakar999@gmail.com>
2021-10-08 06:18:41 +0530

    Armstrong Number

--- src/ArmstrongNumber.java
+++ src/ArmstrongNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.lang.Math;
 public class ArmstrongNumber {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
@@ -7,15 +7,25 @@ public class ArmstrongNumber {
         int  sum=0;
         int n=sc.nextInt();
         int m = n;
+        int te=n;
+
+        int counter=0;
+        int temp;
+        for(;m!=0;)
+        {
+            temp=m%10;
+            counter++;
+            m/=10;
+        }
         while(n!=0){
             int r=n%10;
-            sum=sum+(r*r*r);
+            sum=(int)(sum+(Math.pow(r,counter)));
             n=n/10;
 
         }
         System.out.println(sum);
 
-        if (m==sum) {
+        if (te==sum) {
             System.out.println("its an ARMSTRONG NUMBER");
         }
             else{
